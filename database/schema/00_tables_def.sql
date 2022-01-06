0d94bea6753e9d65671e275934e40885a0eadbbe
Hoang Xuan Bac <62278951+bacxhoang@users.noreply.github.com>
2022-01-07 00:34:00 +0700

    Update 00_tables_def.sql

--- database/schema/00_tables_def.sql
+++ database/schema/00_tables_def.sql
@@ -1,20 +1,21 @@
 
  
  CREATE TABLE IF NOT EXISTS Author (
-	Author_Id INT NOT NULL,
+	Author_Id INT AUTO_INCREMENT NOT NULL,
     Author_Name VARCHAR(50) NOT NULL,
     
     PRIMARY KEY (Author_Id)
 );
 
  CREATE TABLE IF NOT EXISTS Category (
-	Cat_Id INT NOT NULL,
+	Cat_Id INT AUTO_INCREMENT NOT NULL,
     Cat_Name VARCHAR(50) NOT NULL,
     
     PRIMARY KEY (Cat_Id)
 );
 
 CREATE TABLE IF NOT EXISTS Book (
+  Book_Id INT AUTO_INCREMENT NOT NULL,
   ISBN INT NOT NULL,
   Title VARCHAR(100),
   Book_language VARCHAR(20),
@@ -23,21 +24,25 @@ CREATE TABLE IF NOT EXISTS Book (
   No_Copy_Actual INT,
   No_Copy_Current INT,
   
-  PRIMARY KEY (ISBN),
+  PRIMARY KEY (Book_Id),
+  CONSTRAINT ISBN_unique UNIQUE (ISBN),
   FOREIGN KEY (Category_Id) REFERENCES Category (Cat_Id),
   FOREIGN KEY (Author_Id) REFERENCES Author (Author_Id)
 );
 
  CREATE TABLE IF NOT EXISTS Staff (
+ Staff_PK INT AUTO_INCREMENT NOT NULL,
  Staff_Id INT NOT NULL,
  User_Name VARCHAR(30) NOT NULL,
  Login_Password VARCHAR(16) NOT NULL,
  
- PRIMARY KEY (Staff_Id),
+ PRIMARY KEY (Staff_PK),
+ CONSTRAINT StaffId_Unique UNIQUE (Staff_Id),
  CONSTRAINT Username_Unique UNIQUE (User_Name)
  );
     
 CREATE TABLE IF NOT EXISTS Borrower (
+	Borrower_PK INT AUTO_INCREMENT NOT NULL,
 	Borrower_Id INT NOT NULL,
     Book_Id INT NOT NULL,
     Borrowed_from TIME,
@@ -45,21 +50,24 @@ CREATE TABLE IF NOT EXISTS Borrower (
     Returned_date TIME,
     Issued_by INT,
     
-    PRIMARY KEY (Borrower_Id),
-    FOREIGN KEY (Book_Id) REFERENCES Book (ISBN) ON DELETE CASCADE,
-    FOREIGN KEY (Issued_by) REFERENCES Staff (Staff_Id) ON DELETE CASCADE
+    PRIMARY KEY (Borrower_PK),
+	CONSTRAINT BorrowerId_Unique UNIQUE (Borrower_Id),
+    FOREIGN KEY (Book_Id) REFERENCES Book (Book_Id) ON DELETE CASCADE,
+    FOREIGN KEY (Issued_by) REFERENCES Staff (Staff_PK) ON DELETE CASCADE
 );
     
 
  
   CREATE TABLE IF NOT EXISTS Student (
+ Student_PK INT AUTO_INCREMENT NOT NULL,
  Student_Id INT NOT NULL,
  Student_Name VARCHAR(50) NOT NULL,
  Borrower_Id INT,
  Phone_Number INT,
  
- PRIMARY KEY (Student_Id),
- FOREIGN KEY (Borrower_Id) REFERENCES Borrower (Borrower_Id) ON DELETE CASCADE,
+ PRIMARY KEY (Student_PK),
+ FOREIGN KEY (Borrower_Id) REFERENCES Borrower (Borrower_PK) ON DELETE CASCADE,
+ CONSTRAINT StudentId_Unique UNIQUE (Student_Id),
  CONSTRAINT Borrower_Unique UNIQUE (Borrower_Id),
  CONSTRAINT Phonenumber_Unique UNIQUE (Phone_Number)
  );
