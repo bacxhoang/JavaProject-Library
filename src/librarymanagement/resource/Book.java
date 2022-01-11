767ff2a1fec7b55ae5d33325bd08dea16354992a
Hoang Xuan Bac <62278951+bacxhoang@users.noreply.github.com>
2022-01-11 21:24:27 +0700

    Resources updated

--- src/librarymanagement/resource/Book.java
+++ src/librarymanagement/resource/Book.java
@@ -1,5 +1,43 @@
 package librarymanagement.resource;
 
-public class Book extends Resource {
-
+public class Book{
+	private int isbn;
+	private String title;
+	private String language;
+	private int authorId;
+	private int categoryId;
+	private int noCopyActual;
+	private int noCopyCurrent;
+	
+	public Book(int isbn,String title,String language,int authorId,int categoryId, int noCopyActual, int noCopyCurrent) {
+		this.isbn = isbn;
+		this.title = title;
+		this.language = language;
+		this.authorId = authorId;
+		this.categoryId = categoryId;
+		this.noCopyActual = noCopyActual;
+		this.noCopyCurrent = noCopyCurrent;
+	}
+	public int getIsbn(){
+		return isbn;
+		}
+	public String getTitle() {
+		return title;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public int getAuthorId() {
+		return authorId;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public int getNoCopyActual() {
+		return noCopyActual;
+	}
+	public int getNoCopyCurrent() {
+		return noCopyCurrent;
+	}
+	
 }
--- src/librarymanagement/resource/Borrower.java
+++ src/librarymanagement/resource/Borrower.java
@@ -1,5 +1,46 @@
 package librarymanagement.resource;
 
-public class Borrower extends Resource {
+import java.sql.Time;
+import java.sql.Date;
 
+public class Borrower {
+	private int borrowerId;
+	private int bookId;
+	private Time borrowedFrom;
+	private Time borrowedTo;
+	private Date returnedDate;
+	private int issuedBy;
+	
+	public Borrower(int borrowerId,int bookId,Time borrowedFrom,Time borrowedTo,Date returnedDate,int issuedBy) {
+		this.borrowerId = borrowerId;
+		this.bookId = bookId;
+		this.borrowedFrom = borrowedFrom;
+		this.borrowedTo = borrowedTo;
+		this.returnedDate = returnedDate;
+		this.issuedBy = issuedBy;
+	}
+	
+	public int getborrowerId() {
+		return borrowerId;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public Time getBorrowedFrom() {
+		return borrowedFrom;
+	}
+
+	public Time getBorrowedTo() {
+		return borrowedTo;
+	}
+
+	public Date getReturnedDate() {
+		return returnedDate;
+	}
+
+	public int getIssuedBy() {
+		return issuedBy;
+	}
 }
