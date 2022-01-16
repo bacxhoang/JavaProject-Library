f8841e68ea4ed035b520bfb3647932c5d66feb53
Hoang Xuan Bac <62278951+bacxhoang@users.noreply.github.com>
2022-01-16 22:41:00 +0700

    Remove api.java and resourceapi.java

--- src/librarymanagement/api/Api.java
+++
@@ -1,5 +0,0 @@
-package librarymanagement.api;
-
-public class Api {
-
-}
--- src/librarymanagement/api/ResourceApi.java
+++
@@ -1,8 +0,0 @@
-package librarymanagement.api;
-
-import librarymanagement.database.SQLCustomException;
-public class ResourceApi extends Api{
-	
-
-
-}
