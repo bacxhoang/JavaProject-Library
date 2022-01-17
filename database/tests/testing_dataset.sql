547126e711a8ea8e771a5861a7a68fdb69e2ccb0
Hoang Xuan Bac <62278951+bacxhoang@users.noreply.github.com>
2022-01-17 23:33:01 +0700

    Update testing_dataset.sql

--- database/tests/testing_dataset.sql
+++ database/tests/testing_dataset.sql
@@ -23,16 +23,18 @@ CALL createBook('103', 'The Shining','English','2','14','10','10', @statusCode);
 CALL createBook('104', 'Cujo','English','2','14','3','3', @statusCode);
 CALL createBook('105', 'The Adventure of Tom Sawyer','English','1','16','15','15', @statusCode);
 CALL createBook('106', 'A Christmas Carol','English','4','18','5','5', @statusCode);
--- Add into Database Borrowers --
-CALL createBorrower('1001', '101','2022-1-16','2022-2-15',NULL,'13298', @statusCode);
-CALL createBorrower('1002', '102','2022-1-16','2022-2-15',NULL,'13298', @statusCode);
-CALL createBorrower('1003', '101','2022-1-16','2022-2-15',NULL,'13298', @statusCode);
-CALL createBorrower('1004', '103','2022-1-16','2022-2-15',NULL,'13298', @statusCode);
-CALL createBorrower('1005', '103','2022-1-16','2022-2-15',NULL,'13298', @statusCode);
 -- Add into Database Students --
-CALL createStudent('10001', 'Peter Parker','1001','0123456789', @statusCode);
-CALL createStudent('10002', 'Ned Leeds','1002','1234567890', @statusCode);
-CALL createStudent('10003', 'Marry Jane','1003','1237894560', @statusCode);
-CALL createStudent('10004', 'Gwen Stacy','1004','0987654321', @statusCode);
-CALL createStudent('10005', 'Barry Allen','1005','025917782', @statusCode);
+CALL createStudent('1001', 'Peter Parker','10001','0123456789', @statusCode);
+CALL createStudent('1002', 'Ned Leeds','10002','1234567890', @statusCode);
+CALL createStudent('1003', 'Marry Jane','10003','1237894560', @statusCode);
+CALL createStudent('1004', 'Gwen Stacy','10004','0987654321', @statusCode);
+CALL createStudent('1005', 'Barry Allen','10005','025917782', @statusCode);
+-- Add into Database Borrowers --
+CALL createBorrower('10001', '101','2022-1-16','2022-2-15','13298', @statusCode);
+CALL createBorrower('10001', '104','2022-1-16','2022-2-15','13298', @statusCode);
+CALL createBorrower('10002', '102','2022-1-16','2022-2-15','13298', @statusCode);
+CALL createBorrower('10003', '101','2022-1-16','2022-2-15','13298', @statusCode);
+CALL createBorrower('10004', '103','2022-1-16','2022-2-15','13298', @statusCode);
+CALL createBorrower('10005', '103','2022-1-16','2022-2-15','13298', @statusCode);
+
 
